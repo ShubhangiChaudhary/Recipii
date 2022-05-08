e952d9c08ba0e0af5c5f1f2dfe4a53f65a06df4c
= <=>
2022-05-08 23:47:34 +0530

    Feat : User Authentication

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { useUser } from "@auth0/nextjs-auth0";
+
 export default function Home() {
+  const { user, error, loading } = useUser();
   return (
-    <></>
-  )
+    <>
+      {user && (
+        <div>
+          <a href="/api/auth/logout">Logout</a>
+          <div>{user.email}</div>
+          <div>{user.name}</div>
+          <img src={user.picture} />
+        </div>
+      )}
+      {!user && (
+        <div>
+          <a href="/api/auth/login">Login</a>
+        </div>
+      )}
+    </>
+  );
 }
