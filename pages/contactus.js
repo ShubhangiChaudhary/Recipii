7ce709ec3c3b2ac337c043ca1979a322c675904e
ShubhangiChaudhary <84733938+ShubhangiChaudhary@users.noreply.github.com>
2022-05-10 09:39:52 +0530

    Add files via upload
    linked with css page

--- pages/contactus.js
+++ pages/contactus.js
@@ -1,11 +1,30 @@
 import link from "next/link";
+import styles from "../styles/contactus.module.css";
 const contactus = () => {
   return (
-    <>
-      <div>
-        <h1>Contact us</h1>
+    <main>
+      <div class="section">
+        <div className={styles.navbar}>
+          <nav>
+            <div class="logo">
+              <img src="/logo.png" />
+            </div>
+            <ul>
+              <li>
+                <a href="/home" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/aboutus" className="nav-link">
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </>
+    </main>
   );
 };
 
